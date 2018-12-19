611b50d66de2fc77f6a17702ddf9f5365893cb80
Singam ajay <singamay@mail.uc.edu>
2018-12-19 13:34:37 -0500

    initial commit

---
+++ src/main/java/ajay/springframwork/ajcricheroes/AjCricHeroesApplication.java
@@ -0,0 +1,14 @@
+package ajay.springframwork.ajcricheroes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AjCricHeroesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AjCricHeroesApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/ajay/springframwork/ajcricheroes/AjCricHeroesApplicationTests.java
@@ -0,0 +1,17 @@
+package ajay.springframwork.ajcricheroes;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AjCricHeroesApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
